bfbefb93d46e1b47097c348db5d042fa2a2200ed
Ruben Hume <ruben.hume@live.se>
2021-03-14 23:43:12 +0000

    Added missing login handlers to the login page

--- pages/index.js
+++ pages/index.js
@@ -43,10 +43,10 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <Form>
+            <Form onSubmit={handleSubmit(handleLogin)}>
                 <Form.Group controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control ref={register} type="email" name="email" placeholder="Enter email" />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -54,7 +54,7 @@ export default function Home() {
 
                 <Form.Group controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control ref={register} type="password" name="password" placeholder="Password" />
                 </Form.Group>
                 <Form.Group controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Remember Me" />
